c17e6f993a496dcbc11fa520e7d78f78ffd36c64
takc923 <takc923@gmail.com>
2014-01-10 00:01:03 +0900

    toggle add and sub button.

--- nicoplaybutton.js
+++ nicoplaybutton.js
@@ -2,16 +2,23 @@
 var menu = document.querySelector(".siteHeaderMenuList");
 var registerButton = document.createElement('li');
 var anchor = document.createElement('a');
-var img = document.createElement('img');
-img.src = chrome.extension.getURL("icon/add.png");
-img.height = "17";
+var imgAdd = makeButton("icon/add.png", "inline");
+var imgSub = makeButton("icon/sub.png", "none");
 anchor.href = 'javascript:void(0);';
 anchor.addEventListener('click', function(){
     chrome.runtime.sendMessage({
         action : "register"
     });
+    toggleDisplay();
+
+    function toggleDisplay() {
+        var tmp = imgAdd.style.display;
+        imgAdd.style.display = imgSub.style.display;
+        imgSub.style.display = tmp;
+    }
 });
-anchor.appendChild(img);
+anchor.appendChild(imgAdd);
+anchor.appendChild(imgSub);
 registerButton.appendChild(anchor);
 menu.appendChild(registerButton);
 
@@ -25,4 +32,12 @@ var player = document.getElementById('external_nicoplayer');
 
 function toggle() {
     player.ext_play(player.ext_getStatus() !== "playing");
+}
+
+function makeButton(path, display) {
+    var img = document.createElement('img');
+    img.src = chrome.extension.getURL(path);
+    img.height = "17";
+    img.style.display = display;
+    return img;
 }
