062c511b25d951c788cfc26503bd85a4ed0193ca
takc923 <takc923@gmail.com>
2014-01-12 03:27:49 +0900

    handle error when there are no tabs.

--- background.js
+++ background.js
@@ -9,12 +9,13 @@ chrome.runtime.onMessage.addListener(
 chrome.browserAction.onClicked.addListener(function(){
     if (! tabId) return;
 
-    // todo: tabidがなかった場合の処理
     chrome.tabs.sendMessage(
         tabId,
         { action: "toggle" },
         function(isPlaying) {
-            if(isPlaying) {
+            if(chrome.runtime.lastError) {
+                chrome.browserAction.setIcon({path: "icon/play_grey.png"});
+            }else if(isPlaying) {
                 chrome.browserAction.setIcon({path: "icon/play_black.png"});
             } else {
                 chrome.browserAction.setIcon({path: "icon/pause.png"});
